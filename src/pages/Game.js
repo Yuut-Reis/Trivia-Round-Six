3a7c6735ed91da431b2ce13b6c2e7efa9576a65b
Yuut <ingride.reis.b@gmail.com>
2022-04-05 18:38:05 -0300

    :sparkles: feat: Requisito 3

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 class Game extends Component {
-  state = { }
-
   render() {
     return (
-      <h1>Jogo</h1>
+      <div>
+        <h1>Jogo</h1>
+      </div>
     );
   }
 }
