c649c8b11adb053c9ea220bc9a295e071b3c8f85
Jovane Castro <jovanecastro12@gmail.com>
2022-04-08 18:28:54 -0300

    :recycle: refactor: Componente header

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,35 +1,12 @@
-import md5 from 'crypto-js/md5';
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Feedback extends Component {
-  state = { }
-
   render() {
-    const { gravatarEmail, score, name } = this.props;
-    const imgSrc = md5(gravatarEmail).toString();
     return (
-      <header data-testid="feedback-text">
-        Feedback
-        <img src={ imgSrc } alt="profile" data-testid="header-profile-picture" />
-        <p data-testid="header-player-name">{ name }</p>
-        <p data-testid="header-score">{ score }</p>
-      </header>
+      <Header />
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  gravatarEmail: state.player.email,
-  score: state.player.score,
-  name: state.player.name,
-});
-
-Feedback.propTypes = {
-  gravatarEmail: PropTypes.string.isRequired,
-  score: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps, null)(Feedback);
+export default Feedback;
