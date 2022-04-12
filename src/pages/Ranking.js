7a81b6cf09c1b721747c0a70d7633e41e7422e36
Yuut <ingride.reis.b@gmail.com>
2022-04-12 20:37:18 -0300

    :sparkles: feat: requisito 17

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <h1 data-testid="ranking-title"> Ranking </h1>
+      <>
+        <h1 data-testid="ranking-title"> Ranking </h1>
+        <Link to="/">
+          <input type="button" data-testid="btn-go-home" value="Play Again" />
+        </Link>
+      </>
     );
   }
 }
